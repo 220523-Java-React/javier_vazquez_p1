f280487867073b19283f8c771d823a54242497e5
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2022-06-14 08:13:37 -0700

    setup javalin server to run on port 8080

--- src/main/java/com/carDealership/Driver.java
+++ src/main/java/com/carDealership/Driver.java
@@ -1,5 +1,11 @@
 package com.carDealership;
 
+import io.javalin.Javalin;
+
 public class Driver {
-    
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(8080);
+
+        app.get("/", ctx -> ctx.result("Hello Welcome to Javy's Dealership"));
+    }
 }
