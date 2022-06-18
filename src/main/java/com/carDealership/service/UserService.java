fa027f8c1e700e959fbee995811de315c60a63d7
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2022-06-17 22:26:46 -0700

    created user service

---
+++ src/main/java/com/carDealership/service/UserService.java
@@ -0,0 +1,39 @@
+package com.carDealership.service;
+
+import com.carDealership.model.User;
+import com.carDealership.repository.UserRepository;
+
+import java.util.List;
+
+public class UserService {
+
+    UserRepository userRepository;
+
+    public UserService() {
+        userRepository = new UserRepository();
+    };
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    };
+
+    public User createUser(User user) {
+        return userRepository.create(user);
+    };
+
+    public List<User> getAllUsers() {
+        return userRepository.getAll();
+    };
+
+    public User getUserById(long id) {
+        return userRepository.getById(id);
+    };
+
+    public User updateUserByParams(User user, String[] params) {
+        return userRepository.updateByParams(user, params);
+    };
+
+    public User deleteUserById(long id) {
+        return userRepository.deleteById(id);
+    };
+};
