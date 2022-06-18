080ef05cdea0a39418e57278fc89ea532eebc6d3
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2022-06-17 22:36:09 -0700

    created car services

---
+++ src/main/java/com/carDealership/service/CarService.java
@@ -0,0 +1,39 @@
+package com.carDealership.service;
+
+import com.carDealership.model.Car;
+import com.carDealership.repository.CarRepository;
+
+import java.util.List;
+
+public class CarService {
+
+    CarRepository carRepository;
+
+    public CarService() {
+        carRepository = new CarRepository();
+    };
+
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    };
+
+    public Car createCar(Car car) {
+        return carRepository.create(car);
+    };
+
+    public List<Car> getAllCars() {
+        return carRepository.getAll();
+    };
+
+    public Car getCarById(long id) {
+        return carRepository.getById(id);
+    };
+
+    public Car updateCarByParams(Car car, String[] params) {
+        return carRepository.updateByParams(car, params);
+    };
+
+    public Car deleteCarById(long id) {
+        return carRepository.deleteById(id);
+    };
+};
