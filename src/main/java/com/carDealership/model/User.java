e0ac77e975bd91e1a9683f65b96b9c341eb39c39
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2022-06-22 15:29:18 -0700

    changed User model setters to return a User

--- src/main/java/com/carDealership/model/User.java
+++ src/main/java/com/carDealership/model/User.java
@@ -11,71 +11,78 @@ public class User {
 
     public User(){};
 
-    public User(int id, String firstName, String lastName, String username, String password, String email, Role role) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.role = role;
-    };
+//    public User(int id, String firstName, String lastName, String username, String password, String email, Role role) {
+//        this.id = id;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.username = username;
+//        this.password = password;
+//        this.email = email;
+//        this.role = role;
+//    };
 
     public long getId() {
         return id;
-    }
+    };
 
-    public void setId(int id) {
+    public User setId(int id) {
         this.id = id;
-    }
+        return this;
+    };
 
     public String getFirstName() {
         return firstName;
-    }
+    };
 
-    public void setFirstName(String firstName) {
+    public User setFirstName(String firstName) {
         this.firstName = firstName;
-    }
+        return this;
+    };
 
     public String getLastName() {
         return lastName;
-    }
+    };
 
-    public void setLastName(String lastName) {
+    public User setLastName(String lastName) {
         this.lastName = lastName;
-    }
+        return this;
+    };
 
     public String getUsername() {
         return username;
-    }
+    };
 
-    public void setUsername(String username) {
+    public User setUsername(String username) {
         this.username = username;
-    }
+        return this;
+    };
 
     public String getPassword() {
         return password;
-    }
+    };
 
-    public void setPassword(String password) {
+    public User setPassword(String password) {
         this.password = password;
-    }
+        return this;
+    };
 
     public String getEmail() {
         return email;
-    }
+    };
 
-    public void setEmail(String email) {
+    public User setEmail(String email) {
         this.email = email;
-    }
+        return this;
+    };
 
     public Role getRole() {
         return role;
-    }
+    };
 
-    public void setRole(Role role) {
+    public User setRole(Role role) {
         this.role = role;
-    }
+        return this;
+    };
 }
 
 enum Role {
@@ -87,5 +94,5 @@ enum Role {
 
     Role(String value) {
         this.value = value;
-    }
-}
+    };
+};
